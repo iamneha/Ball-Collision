7b5026e07bdcde97293f66ef6b8bf21fd63af9c2
Neha Yadav <inehayadav28@gmail.com>
2016-11-14 22:27:27 +0530

    collision detection

--- script/game.js
+++ script/game.js
@@ -23,7 +23,7 @@ var bricks = [];
 for(c=0; c<brickColumnCount; c++){
 	bricks[c] = [];
 	for(r=0; r<brickRowCount; r++) {
-		bricks[c][r] = {x : 0, y : 0};
+		bricks[c][r] = {x : 0, y : 0, status: 1};
 	}
 }
 
@@ -62,6 +62,7 @@ function draw() {
 	drawBall();
 	drawPaddle();
 	drawBricks();
+	collisionDetection();
 
 	//There are four walls to bounce the ball off.
 	if(y + dy < ballRadius) {  //check with top edge.
@@ -103,15 +104,30 @@ function drawPaddle() {
 function drawBricks() {
 	for(c=0; c<brickColumnCount; c++) {
 		for(r=0; r<brickRowCount; r++) {
-			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-			bricks[c][r].x = brickX;
-			bricks[c][r].y = brickY;
-			ctx.beginPath();
-			ctx.rect(brickX, brickY, brickWidth, brickHeight);
-			ctx.fillStyle = "#0095DD";
-			ctx.fill();
-			ctx.closePath();
+			if(bricks[c][r].status == 1) {
+				var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+				var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}
+		}
+	}
+}
+function collisionDetection() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var b = bricks[c][r];
+			if(b.status == 1) {
+				if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+					dy = -dy;
+					b.status = 0;
+				}
+			}
 		}
 	}
 }
