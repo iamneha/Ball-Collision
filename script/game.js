a4e0a57130685eb9360cbe8cd48003cf8c566ec2
Neha Yadav <inehayadav28@gmail.com>
2016-11-14 20:37:51 +0530

    Add paddle

--- script/game.js
+++ script/game.js
@@ -4,10 +4,37 @@ var x = canvas.width/2;   // Bottom center part of canvas in variables called x 
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
 
 function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -16,8 +43,34 @@ function drawBall() {
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+	drawPaddle();
+
+	//There are four walls to bounce the ball off.
+	if(y + dy < ballRadius || y + dy > canvas.height-ballRadius) {  //check with top left and bottom edge
+		dy = -dy;  //Reverse the direction of ball
+	}
+
+	if(x + dx < ballRadius || x + dx > canvas.width-ballRadius) {  //check with left and right ones.                             
+		dx = -dx;
+	}
+
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+		paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0) {
+		paddleX -= 7;
+	}
+
 	x += dx;
 	y += dy;
+}
+
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
 }
 
 setInterval(draw, 25);
